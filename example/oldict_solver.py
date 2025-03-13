5d0b2de4311fce7f49ba936e314b246e60c0fb14
hyomin <miamiq0000@gmail.com>
2025-03-14 00:58:32 +0900

    x

--- example/oldict_solver.py
+++ example/oldict_solver.py
@@ -5,7 +5,7 @@ G = load_data()
 solver = DEGGraphSolver(G)
 solver.classify_winlose()
 
-# wins = sorted(solver.get_nodes_by_type(WIN))
+wins = sorted(solver.get_nodes_by_type(WIN))
 # print("".join(wins))
 # print("".join(sorted(solver.get_nodes_by_type(LOSE))))
 # print("".join(sorted(solver.get_nodes_by_type(None))))
